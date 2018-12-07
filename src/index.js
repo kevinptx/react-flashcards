98d355f61b1e0bfcbc56d4c0575e48020e2ab4ef
Kevin Peery <kevin.peery@gmail.com>
2018-12-07 13:51:49 -0700

    routes updated

--- src/index.js
+++ src/index.js
@@ -7,11 +7,8 @@ import "semantic-ui-css/semantic.min.css";
 
 ReactDOM.render(
   <Router>
-    <Routes /> 
+    <Routes />
   </Router>,
-document.getElementById("root"));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+  document.getElementById("root")
+);
 serviceWorker.unregister();
